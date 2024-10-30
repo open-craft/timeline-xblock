6d0db487db8386aaf0a46524b7a6d4f0225ea399
Pooja Kulkarni <13742492+pkulkark@users.noreply.github.com>
2024-10-30 11:15:45 -0400

    chore: Change license to Apache 2.0 (#2)

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ def package_data(pkg, roots):
 setup(
     name='timeline-xblock',
     version='0.1',
-    description='timeline XBlock',   # TODO: write a better description.
-    license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
+    description='timeline XBlock',
+    license='Apache 2.0',
     packages=[
         'timeline',
     ],
